60c0f46328a1bb46721aa481d14b771e3051a19a
Mika Tuupola <tuupola@appelsiini.net>
2021-09-11 23:56:27 +0300

    Sync register names from datasheet

--- axp173.h
+++ axp173.h
@@ -28,7 +28,7 @@ This file is part of hardware agnostic I2C driver for AXP173:
 https://github.com/tuupola/axp173
 
 SPDX-License-Identifier: MIT
-Version: 0.6.0-dev
+Version: 0.1.0-dev
 
 */
 
@@ -45,6 +45,7 @@ extern "C" {
 
 /* Power control registers */
 #define AXP173_POWER_STATUS             (0x00)
+/* Power mode/charge status register */
 #define AXP173_CHARGE_STATUS            (0x01)
 #define AXP173_OTG_VBUS_STATUS          (0x04)
 #define AXP173_DATA_BUFFER0             (0x06)
@@ -53,69 +54,69 @@ extern "C" {
 #define AXP173_DATA_BUFFER3             (0x09)
 #define AXP173_DATA_BUFFER4             (0x0a)
 #define AXP173_DATA_BUFFER5             (0x0b)
-/* Output control: 2 EXTEN, 0 DCDC2 */
+/* EXTEN & DC-DC2 switch control register */
 #define AXP173_EXTEN_DCDC2_CONTROL      (0x10)
-/* Power output control: 6 EXTEN, 4 DCDC2, 3 LDO3, 2 LDO2, 1 DCDC3, 0 DCDC1 */
-#define AXP173_DCDC13_LDO23_CONTROL     (0x12)
+/* Power output control: DC-DC1/LDO4 & LDO2/3 switch control register */
+#define AXP173_DCDC1_LDO234_CONTROL     (0x12)
 #define AXP173_DCDC2_VOLTAGE            (0x23)
 #define AXP173_DCDC2_SLOPE              (0x25)
 #define AXP173_DCDC1_VOLTAGE            (0x26)
-#define AXP173_DCDC3_VOLTAGE            (0x27)
-/* Output voltage control: 7-4 LDO2, 3-0 LDO3 */
+#define AXP173_LDO4_VOLTAGE             (0x27)
+/* LDO2/3 voltage setting register */
 #define AXP173_LDO23_VOLTAGE            (0x28)
 #define AXP173_VBUS_IPSOUT_CHANNEL      (0x30)
 #define AXP173_SHUTDOWN_VOLTAGE         (0x31)
 #define AXP173_SHUTDOWN_BATTERY_CHGLED_CONTROL (0x32)
 #define AXP173_CHARGE_CONTROL_1         (0x33)
 #define AXP173_CHARGE_CONTROL_2         (0x34)
-#define AXP173_BATTERY_CHARGE_CONTROL   (0x35)
+//#define AXP173_BATTERY_CHARGE_CONTROL   (0x35)
 #define AXP173_PEK                      (0x36)
 #define AXP173_DCDC_FREQUENCY           (0x37)
 #define AXP173_BATTERY_CHARGE_LOW_TEMP  (0x38)
 #define AXP173_BATTERY_CHARGE_HIGH_TEMP (0x39)
-#define AXP173_APS_LOW_POWER1           (0x3A)
-#define AXP173_APS_LOW_POWER2           (0x3B)
+#define AXP173_APS_LOW_POWER1           (0x3a)
+#define AXP173_APS_LOW_POWER2           (0x3b)
 #define AXP173_BATTERY_DISCHARGE_LOW_TEMP  (0x3c)
 #define AXP173_BATTERY_DISCHARGE_HIGH_TEMP (0x3d)
 #define AXP173_DCDC_MODE                (0x80)
 #define AXP173_ADC_ENABLE_1             (0x82)
 #define AXP173_ADC_ENABLE_2             (0x83)
 #define AXP173_ADC_RATE_TS_PIN          (0x84)
-#define AXP173_GPIO30_INPUT_RANGE       (0x85)
-#define AXP173_GPIO0_ADC_IRQ_RISING     (0x86)
-#define AXP173_GPIO0_ADC_IRQ_FALLING    (0x87)
+// #define AXP173_GPIO30_INPUT_RANGE       (0x85)
+// #define AXP173_GPIO0_ADC_IRQ_RISING     (0x86)
+// #define AXP173_GPIO0_ADC_IRQ_FALLING    (0x87)
 #define AXP173_TIMER_CONTROL            (0x8a)
 #define AXP173_VBUS_MONITOR             (0x8b)
 #define AXP173_TEMP_SHUTDOWN_CONTROL    (0x8f)
 
 /* GPIO control registers */
-#define AXP173_GPIO0_CONTROL            (0x90)
-#define AXP173_GPIO0_LDOIO0_VOLTAGE     (0x91)
-#define AXP173_GPIO1_CONTROL            (0x92)
-#define AXP173_GPIO2_CONTROL            (0x93)
-#define AXP173_GPIO20_SIGNAL_STATUS     (0x94)
-#define AXP173_GPIO43_FUNCTION_CONTROL  (0x95)
-#define AXP173_GPIO43_SIGNAL_STATUS     (0x96)
-#define AXP173_GPIO20_PULLDOWN_CONTROL  (0x97)
-#define AXP173_PWM1_FREQUENCY           (0x98)
-#define AXP173_PWM1_DUTY_CYCLE_1        (0x99)
-#define AXP173_PWM1_DUTY_CYCLE_2        (0x9a)
-#define AXP173_PWM2_FREQUENCY           (0x9b)
-#define AXP173_PWM2_DUTY_CYCLE_1        (0x9c)
-#define AXP173_PWM2_DUTY_CYCLE_2        (0x9d)
-#define AXP173_N_RSTO_GPIO5_CONTROL     (0x9e)
+// #define AXP173_GPIO0_CONTROL            (0x90)
+// #define AXP173_GPIO0_LDOIO0_VOLTAGE     (0x91)
+// #define AXP173_GPIO1_CONTROL            (0x92)
+// #define AXP173_GPIO2_CONTROL            (0x93)
+// #define AXP173_GPIO20_SIGNAL_STATUS     (0x94)
+// #define AXP173_GPIO43_FUNCTION_CONTROL  (0x95)
+// #define AXP173_GPIO43_SIGNAL_STATUS     (0x96)
+// #define AXP173_GPIO20_PULLDOWN_CONTROL  (0x97)
+// #define AXP173_PWM1_FREQUENCY           (0x98)
+// #define AXP173_PWM1_DUTY_CYCLE_1        (0x99)
+// #define AXP173_PWM1_DUTY_CYCLE_2        (0x9a)
+// #define AXP173_PWM2_FREQUENCY           (0x9b)
+// #define AXP173_PWM2_DUTY_CYCLE_1        (0x9c)
+// #define AXP173_PWM2_DUTY_CYCLE_2        (0x9d)
+// #define AXP173_N_RSTO_GPIO5_CONTROL     (0x9e)
 
 /* Interrupt control registers */
 #define AXP173_ENABLE_CONTROL_1         (0x40)
 #define AXP173_ENABLE_CONTROL_2         (0x41)
 #define AXP173_ENABLE_CONTROL_3         (0x42)
 #define AXP173_ENABLE_CONTROL_4         (0x43)
-#define AXP173_ENABLE_CONTROL_5         (0x4a)
+// #define AXP173_ENABLE_CONTROL_5         (0x4a)
 #define AXP173_IRQ_STATUS_1             (0x44)
 #define AXP173_IRQ_STATUS_2             (0x45)
 #define AXP173_IRQ_STATUS_3             (0x46)
 #define AXP173_IRQ_STATUS_4             (0x47)
-#define AXP173_IRQ_STATUS_5             (0x4d)
+// #define AXP173_IRQ_STATUS_5             (0x4d)
 
 /* ADC data registers */
 #define AXP173_ACIN_VOLTAGE             (0x56)
@@ -124,10 +125,10 @@ extern "C" {
 #define AXP173_VBUS_CURRENT             (0x5c)
 #define AXP173_TEMP                     (0x5e)
 #define AXP173_TS_INPUT                 (0x62)
-#define AXP173_GPIO0_VOLTAGE            (0x64)
-#define AXP173_GPIO1_VOLTAGE            (0x66)
-#define AXP173_GPIO2_VOLTAGE            (0x68)
-#define AXP173_GPIO3_VOLTAGE            (0x6a)
+// #define AXP173_GPIO0_VOLTAGE            (0x64)
+// #define AXP173_GPIO1_VOLTAGE            (0x66)
+// #define AXP173_GPIO2_VOLTAGE            (0x68)
+// #define AXP173_GPIO3_VOLTAGE            (0x6a)
 #define AXP173_BATTERY_POWER            (0x70)
 #define AXP173_BATTERY_VOLTAGE          (0x78)
 #define AXP173_CHARGE_CURRENT           (0x7a)
